179e015d4d3a9e0811d260481569953caa2d9760
raimondas-27 <raimondasvilnius@yahoo.com>
2021-06-28 12:01:04 +0300

    tasks imported

--- functions/promise.test.js
+++ functions/promise.test.js
@@ -7,3 +7,9 @@ test("email should be Shanna@melissa.tv", () => {
       expect(data.email).toBe("Shanna@melissa.tv")
    })
 })
+
+test("email should be Shanna@melissa.tv ASYNC option", async () => {
+   expect.assertions(1);
+   const data = await getUser()
+   expect(data.email).toBe("Shanna@melissa.tv")
+})
